8cfd21a6d8430f36905a1fa848456b66252b844e
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-15 22:07:42 -0400

    camera working on screen

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
 
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//take the user to a unique uuid param
 app.get("/", (req, res) => {
-  res.render("room");
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
 });
 
 const port = process.env.PORT || 3000;
