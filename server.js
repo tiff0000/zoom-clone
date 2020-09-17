b8a48fb55a7df311ccd9ba997ee4ea4bd929eaf0
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-17 10:34:25 -0400

    input styling

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-//take the user to a unique uuid param
+// take the user to a unique uuid param
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
