7a785573d0c393e4f173409053a6abe3b12d0813
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-16 23:43:59 -0400

    added peerjs webrtconnected user

--- public/script.js
+++ public/script.js
@@ -1,6 +1,15 @@
+const { PeerServer } = require("peer");
+
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+
+var peer = new PeerServer(undefined, {
+  path: "./peerjs",
+  host: "/",
+  port: "3030",
+});
 
 let myVideoStream;
 // get video from the chrome
@@ -13,6 +22,19 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
   });
+
+peer.on("open", (id) => {
+  // join with a specific room id
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+socket.on("user-connected", (userId) => {
+  connectToNewUser(userId, stream);
+});
+
+const connectToNewUser = () => {
+  console.log("connected new user");
+};
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const { v4: uuidv4 } = require("uuid");
+const { isObject } = require("util");
+const server = require("http").Server(app);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+const io = require("socket.io")(server);
 
+app.use("/peerjs", peerServer);
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -17,4 +25,12 @@ app.get("/:room", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
+// socket config
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
+});
