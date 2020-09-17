23fae17372abe130ae9680318edf25d6a6b9e68b
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-17 00:31:29 -0400

    styling for zoom screen

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-const { v4: uuidv4 } = require("uuid");
-const { isObject } = require("util");
+// const cors = require('cors')
+// app.use(cors())
+const path = require("path");
 const server = require("http").Server(app);
+const io = require("socket.io")(server);
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
-const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
 
 app.use("/peerjs", peerServer);
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,20 +18,29 @@ app.set("view engine", "ejs");
 
 //take the user to a unique uuid param
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+  res.redirect(`/${uuidV4()}`);
 });
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => console.log(`Listening on port ${port}`));
-
 // socket config
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user-connected", userId);
+    // messages
+    socket.on("message", (message) => {
+      //send message to the same room
+      io.to(roomId).emit("createMessage", message);
+    });
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
   });
 });
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
